c77d27cc6667c722c375cab88036b339dbc7b459
Mitchell de Boer <mitchelldeboer@hotmail.com>
2023-07-03 17:22:11 +0200

    new api changes

--- src/VolumeLoaderPlugin.cpp
+++ src/VolumeLoaderPlugin.cpp
@@ -29,7 +29,7 @@ using namespace hdps;
 
 VolumeLoaderPlugin::~VolumeLoaderPlugin(void)
 {
-    
+
 }
 
 /**
@@ -43,7 +43,7 @@ void VolumeLoaderPlugin::init()
 }
 
 /**
- * Funtion the loads in the data and transforms it from its file type to pointsdata. 
+ * Funtion the loads in the data and transforms it from its file type to pointsdata.
  * It also fills in missing data in the input volume with values 1 smaller than the smallest value in the dimension
  * (needed for visualization).
  */
@@ -56,10 +56,10 @@ void VolumeLoaderPlugin::loadData()
     if (QFileInfo(filePath).exists())
         setSetting("Data/WorkingDirectory", QFileInfo(filePath).absoluteDir().absolutePath());
 
-    std::string hdvol = ".hdvol"; 
+    std::string hdvol = ".hdvol";
 
     int found = filePath.toStdString().find(".hdvol");
-    
+
 
     if (found == -1) {
         QMessageBox messageBox;
@@ -80,23 +80,23 @@ void VolumeLoaderPlugin::loadData()
 
         points->getDataHierarchyItem().setTaskRunning();
         points->getDataHierarchyItem().setTaskName("Load volume");
-        
+
         points->getDataHierarchyItem().setTaskDescription("Allocating voxels");
 
         QCoreApplication::processEvents();
-        
+
         int a;
         float b;
         std::ifstream file(filePath.toStdString(), std::ios::in | std::ios::binary);
         file.read((char*)&a, sizeof(a)); // read the number of points as stated in the hdvol metadata (this excludes missing data)
         int numPoints = a;
         file.read((char*)&a, sizeof(a)); // read the number of dimensions stated in the hdvol metadata
-        int numDimensions = a;
+        int numDimensions = a + 3;
 
         std::vector<int16_t> dimensions(6); // creates vector for holding hdvol size data
-        
 
-        for (int i = 0; i < 6;i++) {
+
+        for (int i = 0; i < 6; i++) {
             file.read((char*)&a, sizeof(a));
             dimensions[i] = a; // reads the minimum and maximum xyz values from hdvol metadata in the following order. xMin,xMax,yMin,yMax,zMin,zMax
         }
@@ -108,13 +108,16 @@ void VolumeLoaderPlugin::loadData()
         int zSize = dimensions[5] - dimensions[4];// calculate zSize
 
         // creates a 4D dataVector of size (xSize,ySize,zSize,numDimensions) with inial values 0
-        std::vector<std::vector<std::vector<std::vector<float>>>> dataArray(xSize, std::vector<std::vector<std::vector<float>>>(ySize, std::vector<std::vector<float>>(zSize,std::vector<float>(numDimensions,0))));
+        std::vector<std::vector<std::vector<std::vector<float>>>> dataArray(xSize, std::vector<std::vector<std::vector<float>>>(ySize, std::vector<std::vector<float>>(zSize, std::vector<float>(numDimensions, 0))));
         // creates a 4D binary objectMask vector used for resetting non-Object points (so missing data) back to 0 after scaling has been performed
         std::vector<std::vector<std::vector<std::vector<float>>>> backgroundMask(xSize, std::vector<std::vector<std::vector<float>>>(ySize, std::vector<std::vector<float>>(zSize, std::vector<float>(numDimensions, 1))));
-        
+
         // creates a 1D vector used to read the completed dataset into the hdps points datatype
-        std::vector<float> dataSet((xSize * ySize*zSize* numDimensions));
-        
+        std::vector<float> dataSet((xSize * ySize * zSize * numDimensions));
+
+        std::vector<float> dataSetxyz((xSize * ySize * zSize * 3));
+
+
         // creates 1D vectors of size numDimensions that are used to contain the minimum and maximum values in each dimension for scaling purposes
         std::vector<float> dimensionMinimum(numDimensions, 0);
         std::vector<float> dimensionMaximum(numDimensions, 0);
@@ -122,9 +125,9 @@ void VolumeLoaderPlugin::loadData()
         int zCor;
         int yCor;
         int xCor;
-        int index=0;
+        int index = 0;
         int dataIterator;
-        
+
         QCoreApplication::processEvents();
 
         points->getDataHierarchyItem().setTaskDescription("Loading points");
@@ -135,20 +138,20 @@ void VolumeLoaderPlugin::loadData()
             for (int t = 0; t < 3; t++) { // loop through xyz values of current datapoint
                 file.read((char*)&a, 4);
                 coordinates[t] = a; // Read coordinates off current datapoint
-                
+
             }
             xCor = coordinates[0] - 1;
             yCor = coordinates[1] - 1;
             zCor = coordinates[2] - 1;
-            for (int i = 0; i < numDimensions; i++) // loop through all dimensions of current datapoint
-            {   
+            for (int i = 0; i < numDimensions - 3; i++) // loop through all dimensions of current datapoint
+            {
                 file.read((char*)&b, 4);
 
                 dataArray[xCor][yCor][zCor][i] = b; // read current dimension of current datapoint into the dataVector
                 backgroundMask[xCor][yCor][zCor][i] = 0; // set the current datapoint to 0 for non-missing data
-                
+
                 if (k == 0) {// if its the firs itteration set initial minimum
-                    dimensionMinimum[i] = b; 
+                    dimensionMinimum[i] = b;
                 }
                 if (b < dimensionMinimum[i]) { // otherwise only change the minimum if the current value is lower than the lowest value currently stored
                     dimensionMinimum[i] = b;
@@ -159,7 +162,7 @@ void VolumeLoaderPlugin::loadData()
                 points->getDataHierarchyItem().setTaskProgress(k / static_cast<float>(numPoints));
                 QCoreApplication::processEvents();
             }
-                
+
         }
 
         points->getDataHierarchyItem().setTaskProgress(1.0f);
@@ -168,75 +171,128 @@ void VolumeLoaderPlugin::loadData()
 
         file.close(); // stop reading the file
 
-        
+
         points->getDataHierarchyItem().setTaskDescription("Masking points");
         points->getDataHierarchyItem().setTaskProgress(0.0f);
 
 
-        std::vector<unsigned int, std::allocator<unsigned int>> selectionIndices;
+        std::vector<unsigned int> selectionIndices;
 
         selectionIndices.reserve(xSize * ySize * zSize);
 
         int iterator = 0;
+        int iteratorXyz = 0;
         int selectCount = 0;
-        
+
+
+
+
         //loop through the dataArray to scale all the datapoints and set background to minimum object value -1
         for (int z = 0; z < zSize; z++) {
             for (int y = 0; y < ySize; y++) {
                 for (int x = 0; x < xSize; x++) {
                     for (int dim = 0; dim < numDimensions; dim++) {
-                        
-                        
 
-                        dataSet[iterator] = dataArray[x][y][z][dim]+backgroundMask[x][y][z][dim]*(dimensionMinimum[dim]-1);
-
-                        // record indices containing only Object coordinates
-                        if (dataSet[iterator] != (dimensionMinimum[dim] - 1) && iterator % numDimensions == 0) {
-                            selectionIndices.push_back(iterator/numDimensions);
-                            selectCount++;
+                        if (dim == 0) {
+                            dataSetxyz[iteratorXyz] = x;
+                            iteratorXyz++;
                         }
-                        iterator++;
+                        else if (dim == 1)
+                        {
+                            dataSetxyz[iteratorXyz] = y;
+                            iteratorXyz++;
+                        }
+                        else if (dim == 2) {
+                            dataSetxyz[iteratorXyz] = z;
+                            iteratorXyz++;
+                        }
+                        else {
+                            dataSet[iterator] = dataArray[x][y][z][dim] + backgroundMask[x][y][z][dim] * (dimensionMinimum[dim] - 1);
+                            // record indices containing only Object coordinates
+                            if (backgroundMask[x][y][z][dim - 3] == 0 && dim == 3) {
+                                selectionIndices.push_back(iterator);
+                                selectCount++;
+                            }
+                            iterator++;
+                        }
+
+
                     }
                 }
             }
 
             if ((z % 10) == 0) {
+
                 points->getDataHierarchyItem().setTaskProgress(z / static_cast<float>(zSize));
                 QCoreApplication::processEvents();
             }
-            
-        }
-    
-        selectionIndices.resize(selectCount);
 
-        
+        }
+
+        selectionIndices.resize(selectCount);
+        std::cout << selectionIndices.size() << std::endl;
+
+
         points->getDataHierarchyItem().setTaskProgress(1.0f);
-        
-        points->setData(dataSet.data(), xSize*ySize*zSize, numDimensions);// pass 1D vector to points data in core
-        
+
+        std::cout << "im here" << std::endl;
+        Dataset<Points> xyzData = _core->addDataset("Points", "xyz", points);
+        events().notifyDatasetAdded(xyzData);
+
+        Dataset<Points> colorTest = _core->addDataset<Points>("Points", "colorTest");
+        events().notifyDatasetAdded(colorTest);
+        std::vector<float> testcolor;
+
+        for (int i = 0; i < xSize * ySize * zSize; i++)
+        {
+            if (i < (xSize * ySize * zSize / 3)) {
+                testcolor.push_back(0);
+            }
+            else if (i < (2 * (xSize * ySize * zSize / 3))) {
+                testcolor.push_back(0.5);
+            }
+            else {
+                testcolor.push_back(1);
+            }
+        }
+
+
+
+
+
+        xyzData->setData(dataSetxyz.data(), xSize * ySize * zSize, 3);
+        colorTest->setData(testcolor.data(), xSize * ySize * zSize, 1);
+
+        points->setData(dataSet.data(), xSize * ySize * zSize, numDimensions - 3);// pass 1D vector to points data in core
+
         //declare the x,y and z sizes of the data which is needed to create the imagedata object in the 3D viewer
         points->setProperty("xDim", xSize);
         points->setProperty("yDim", ySize);
         points->setProperty("zDim", zSize);
         std::vector<QString> names = { "xDim", "yDim", "zDim" };
         points->setDimensionNames(names);
-        events().notifyDatasetChanged(points);
+        events().notifyDatasetDataChanged(points);
 
         points->getDataHierarchyItem().setTaskDescription("Creating subset");
 
 
         // create automatic object only subset
         points->setSelectionIndices(selectionIndices);
+
+        //points->setSelec
         auto subset = points->createSubsetFromSelection("Object Only", points);
 
+
         // Notify the core system of the new data
-        events().notifyDatasetAdded(subset);
+
 
         points->selectNone();
 
         points->getDataHierarchyItem().setTaskFinished();
+
+
     }
-    
+
 }
 
 QIcon VolumeLoaderPluginFactory::getIcon(const QColor& color /*= Qt::black*/) const
